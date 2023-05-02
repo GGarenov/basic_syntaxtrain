517dca6d490d7ffa9491c4ef4dd50b45cbc437ab
Georgiy Garenov <g_garenov@outlook.com>
2023-05-02 22:01:57 +0300

    retrain

---
+++ arrays/addAndSubtract.js
@@ -0,0 +1,8 @@
+function addAndSubtract(arr) {
+    for (i = 0; i < arr.length; i++){
+        arr[i] = Number(arr[i]);
+    }
+    
+
+}
+addAndSubtract([5, 15, 23, 56, 35])
--- arrays/evenAndSubtraction.js
+++ arrays/evenAndSubtraction.js
@@ -15,15 +15,15 @@ function evenAndSubstraction(arr) {
             sumOdd += num
         }
     }
-    // if (sumOdd > sumEven) {   <- tozi kod smqna razlikata mejdu chetnoto i nechetnoto
-    //     console.log(sumOdd - sumEven)
-    // } else if (sumEven > sumOdd) {
-    //     console.log(sumEven - sumOdd)
-    // }
+    if (sumOdd > sumEven) {   
+         console.log(sumOdd - sumEven)
+     } else if (sumEven > sumOdd) {
+         console.log(sumEven - sumOdd)
+     }
 
     // NO ima i po-lesen nachin s math.abs
-    let diff = Math.abs(sumEven - sumOdd)
-    console.log(diff)
+    // let diff = Math.abs(sumEven - sumOdd)
+    // console.log(diff)
 }
 evenAndSubstraction([1, 2, 3, 4, 5, 6])
 console.log(`--------------------------------`)
